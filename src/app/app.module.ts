c64f86028b8a956c5f2821b3d7390b8f76e3d72f
NGUYENHAMANH <linhmanhngan@gmail.com>
2022-06-17 17:00:01 +0700

    angular1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {ProductsComponent} from "./DS.SP/products.component";
+import {WeatherComponent} from "./weather/weather.compunent";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+        ProductsComponent,
+    WeatherComponent
+
   ],
   imports: [
     BrowserModule
